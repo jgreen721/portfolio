b0c566573395234921f261b3bb9a743bd90c69ea
Justin Green <justingreen@Justins-Air.lan>
2025-08-20 10:49:28 -0400

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import {Hero} from "./components"
 import './App.css'
 
 function App() {
-  const [theme,setTheme] = useState("")
+  const [theme,setTheme] = useState("light")
 
   return (
-    <div data-them="light" className="app">
+    <div data-them={theme} className="app">
       <div className="app-content">
           <Hero/>
-          kdkdkd
       </div>
     </div>
   )
